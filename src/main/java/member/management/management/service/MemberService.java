a7d20f46bcb1f638a6ea271d775f84e7f61469ff
iamminho <minho100227@kakao.com>
2023-03-16 11:17:56 +0900

    회원 웹 기능 - 등록

--- src/main/java/member/management/management/service/MemberService.java
+++ src/main/java/member/management/management/service/MemberService.java
@@ -1,9 +1,10 @@
 package member.management.management.service;
 
+import member.management.management.domain.Member;
 import member.management.management.repository.MemberReopsitory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
 
 public class MemberService {
     private final MemberReopsitory memberReopsitory;
@@ -11,4 +12,27 @@ public class MemberService {
     public MemberService(MemberReopsitory memberReopsitory) {
         this.memberReopsitory = memberReopsitory;
     }
-} 
+
+//    회원가입
+    public Long join(Member member) {
+        validateDuplicateMember(member);
+        memberReopsitory.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        memberReopsitory.findByName(member.getName())
+                .ifPresent(m -> {
+                    throw new IllegalStateException("이미 존재하는 회원입니다.");
+                });
+    }
+
+//    전체 회원 조회
+    public List<Member> findMembers() {
+        return memberReopsitory.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId) {
+        return memberReopsitory.findById(memberId);
+    }
+}
