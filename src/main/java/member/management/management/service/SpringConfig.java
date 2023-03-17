e4a88b1ea7c5b19fe7f60fbbb52b2c72a169bcf7
iamminho <minho100227@kakao.com>
2023-03-17 10:37:51 +0900

    순수 JDBC

--- src/main/java/member/management/management/service/SpringConfig.java
+++ src/main/java/member/management/management/service/SpringConfig.java
@@ -1,18 +1,30 @@
 package member.management.management.service;
 
+import member.management.management.repository.JdbcMemberRepository;
 import member.management.management.repository.MemberReopsitory;
 import member.management.management.repository.MemoryMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+    private DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
     @Bean
     public MemberService memberService() {
         return new MemberService(memberReopsitory());
     }
     @Bean
     public MemberReopsitory memberReopsitory() {
-        return new MemoryMemberRepository();
+//        return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 }
