9dda02dbde97522595948515d1d758cf36c9aa4a
iamminho <minho100227@kakao.com>
2023-03-18 15:56:47 +0900

    스프링 데이터 JPA

---
+++ src/main/java/member/management/management/repository/SpringDataJpaMemberRepository.java
@@ -0,0 +1,12 @@
+package member.management.management.repository;
+
+import member.management.management.domain.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface SpringDataJpaMemberRepository extends JpaRepository<Member, Long>, MemberRepository {
+
+    @Override
+    Optional<Member> findByName(String name);
+}
